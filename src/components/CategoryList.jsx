ed792e940535d5656b40db15714e5478629843ba
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-04-12 22:33:58 -0300

    increase gap and scale

--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -1,8 +1,8 @@
 export function CategoryList({ categories }) {
   return (
-    <ul className="flex items-center gap-4">
+    <ul className="flex items-center gap-6">
       {categories.map((category) => (
-        <li key={category} className="hover:scale-105 transition-transform">
+        <li key={category} className="hover:scale-110 transition-transform">
           <a href={`/${category}`}>{category}</a>
         </li>
       ))}
