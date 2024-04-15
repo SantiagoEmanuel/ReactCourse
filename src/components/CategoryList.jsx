84c809875a5da26e7265badce3c7aab0d3656d93
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-04-15 20:08:36 -0300

    Add css scale

--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -1,8 +1,8 @@
 import { CustomLink } from "./ui/CustomLink";
 
-export function CategoryList({ categories }) {
+export function CategoryList({ categories, addCss }) {
   return (
-    <ul className="flex items-center gap-6">
+    <ul className={`flex items-center gap-6 ${addCss}`}>
       {categories.map(({ name, url }, index) => (
         <li key={index} className="hover:scale-110 transition-transform">
           <CustomLink to={url} label={name} />
