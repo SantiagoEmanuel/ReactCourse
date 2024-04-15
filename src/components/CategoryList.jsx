2c09cf07b16217611a9641280d8858cdc402b1f6
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-04-14 22:10:23 -0300

    Upgrade component.

--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -1,9 +1,9 @@
 export function CategoryList({ categories }) {
   return (
     <ul className="flex items-center gap-6">
-      {categories.map((category) => (
-        <li key={category} className="hover:scale-110 transition-transform">
-          <a href={`/${category}`}>{category}</a>
+      {categories.map(({ name, url }) => (
+        <li key={name} className="hover:scale-110 transition-transform">
+          <a href={`/${url}`}>{name}</a>
         </li>
       ))}
     </ul>
