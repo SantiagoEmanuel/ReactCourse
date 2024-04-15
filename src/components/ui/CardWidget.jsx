d9323a34447edb08d32378efaf361ecbc5e8bcbe
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-04-15 19:28:10 -0300

    Move to ui folder

---
+++ src/components/ui/CardWidget.jsx
@@ -0,0 +1,28 @@
+import { Typography } from "./Typography";
+
+export function CardWidget({ cartCount = 0 }) {
+  return (
+    <div className="relative cursor-pointer">
+      <img
+        src="/icons/shoppingCart/shoppingCart-black.png"
+        alt="ShoppingCart icon"
+        className="dark:hidden"
+      />
+      <img
+        src="/icons/shoppingCart/shoppingCart.png"
+        alt="ShoppingCart icon"
+        className="dark:block hidden"
+      />
+      {cartCount != 0 ? (
+        <Typography
+          As="span"
+          className="-bottom-1 -right-1 absolute rounded-full bg-[#f14c26] z-10 px-1 text-xs"
+        >
+          {cartCount}
+        </Typography>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+}
