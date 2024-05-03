a0f105e4ac1a6584fcca015f5b1d52b9aa1fa522
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-03 17:31:25 -0300

    Remove double space

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -8,7 +8,7 @@ export function Layout({ children }) {
                     {children}
                </main>
                <footer>
-                    <p className="text-balance  text-sm text-center">The backend is under development now, it works, but needs more optimization and bug fixes.
+                    <p className="text-balance text-sm text-center">The backend is under development now, it works, but needs more optimization and bug fixes.
                          You are using V1 of backend, V2 in development. If you want to help me with the development of the backend, do not hesitate to contact me, let's work as a team.</p>
                     {/* <p className="text-sm text-center">Frontend and Backend created by <span className="text-xl font-bold text-orange-600">Santiago Emanuel</span></p> */}
                </footer>
