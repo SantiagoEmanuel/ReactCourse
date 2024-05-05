1d30459e068137594068e4634b9b6c1c91cf0b4e
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-05 05:02:39 -0300

    Add aside for mobile menu

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,3 +1,6 @@
+import { closeAside } from "../functions/closeAside";
+import { CategoriesList } from "./CategoriesList";
+import { CloseIcon } from "./CloseIcon";
 import { NavBar } from "./NavBar";
 
 export function Layout({ children }) {
@@ -12,6 +15,12 @@ export function Layout({ children }) {
                          You are using V1 of backend, V2 in development. If you want to help me with the development of the backend, do not hesitate to contact me, let's work as a team.</p>
                     {/* <p className="text-sm text-center">Frontend and Backend created by <span className="text-xl font-bold text-orange-600">Santiago Emanuel</span></p> */}
                </footer>
+               <aside className="absolute top-0 right-0 z-10 hidden w-full h-full bg-[#212121] items-center justify-center pt-4 pr-4 showAside">
+                    <button onClick={closeAside} className="top-4 right-4 absolute">
+                         <CloseIcon />
+                    </button>
+                    <CategoriesList css="flex flex-col gap-4 justify-center items-center text-center text-2xl" />
+               </aside>
           </>
      )
 }
