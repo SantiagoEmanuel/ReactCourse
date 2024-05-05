03601426d7c0d3152dc622548b82ff44f8fd17d4
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-05 05:14:24 -0300

    fix bug on menu with width is > 768

--- src/functions/openAside.js
+++ src/functions/openAside.js
@@ -1,9 +1,9 @@
 export const showMenu = () => {
      const mobileMenu = document.querySelector("aside");
-     if (!mobileMenu.classList.contains('flex')) {
+     if (!mobileMenu.classList.contains('max-md:flex')) {
           mobileMenu.classList.remove('hidden')
           mobileMenu.classList.remove('hiddenAside');
-          mobileMenu.classList.add('flex');
+          mobileMenu.classList.add('max-md:flex');
           mobileMenu.classList.add('showAside');
      }
      // mobileMenu.classList.remove('showAside');
