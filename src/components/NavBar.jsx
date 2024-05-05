5bc4d9172f70e725664681a9e225e735d3c16d86
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-05 05:03:04 -0300

    Add responsive for mobile view

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,6 +4,8 @@ import { CartWidget } from "./CartWidget"
 import { useUserContext } from "../context/userContext"
 import { useUserToggleContext } from '../context/userToggleContext'
 import { Link } from "react-router-dom";
+import { MenuIcon } from "./MenuIcon"
+import { showMenu } from "../functions/openAside"
 
 export function NavBar() {
      const user = useUserContext();
@@ -12,14 +14,22 @@ export function NavBar() {
      user?.cart.map(({ count }) => {
           countProduct.push(count)
      })
+
      return (
           <header className="flex items-center justify-between">
                <Brand>E-Commerce {user && <p>Welcome {user.username}!</p>} </Brand>
                <nav className="flex items-center justify-around gap-6">
-                    <CategoriesList />
-                    {user?.status == 'admin' ? <Link to={'/create-product'} className="hover:scale-110 font-bold transition-transform">Create Product</Link> : ''}
-                    {user && <button className="hover:scale-110 font-bold transition-transform" onClick={logOut}>Log out</button>}
-                    <CartWidget count={countProduct} />
+                    <div className="max-md:hidden flex items-center justify-around gap-6">
+                         <CategoriesList />
+                         {user?.status == 'admin' ? <Link to={'/create-product'} className="hover:scale-110 font-bold transition-transform">Create Product</Link> : ''}
+                         {user && <button className="hover:scale-110 font-bold transition-transform" onClick={logOut}>Log out</button>}
+                         <CartWidget count={countProduct} />
+                    </div>
+                    <div className="max-md:flex items-center content-center hidden">
+                         <button onClick={showMenu}>
+                              <MenuIcon />
+                         </button>
+                    </div>
                </nav>
           </header>
      )
