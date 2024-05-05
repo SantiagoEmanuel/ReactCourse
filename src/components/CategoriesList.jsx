c5a23199197df23c31bcb91a6fad7470a3481df7
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-05 05:02:01 -0300

    Add prop css for more flexibility

--- src/components/CategoriesList.jsx
+++ src/components/CategoriesList.jsx
@@ -1,15 +1,14 @@
 import { Link } from "react-router-dom";
 import { useCategories } from "../hook/useCategories";
+import { closeAside } from "../functions/closeAside";
 
-export function CategoriesList() {
-
+export function CategoriesList({ css = "flex items-center gap-4" }) {
      const { categories } = useCategories();
-
      return (
-          <ul className="flex items-center gap-4">
+          <ul className={css}>
                {
                     categories.map(({ name, url }, index) => (
-                         <li key={index} className="hover:scale-110 transition-transform"><Link to={url}>{name}</Link></li>
+                         <li key={index} className="hover:scale-110 transition-transform" onClick={closeAside}><Link to={url}>{name}</Link></li>
                     ))
                }
           </ul>
