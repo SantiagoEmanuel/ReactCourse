69b8296cf54237f7edb18a9600b8872c248a6cec
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-05 08:00:20 -0300

    Elimino codigo que tuve que hacer para recuperar toda la informacion que venia de mi backend, ahora todo viene mas optimo y sin el bug de cargar a veces menos productos.

--- src/functions/getItem.js
+++ src/functions/getItem.js
@@ -1,5 +1,5 @@
 export async function getItem(ids) {
-     const [{ id, title, description, imageUrl, price, stock }, { category }] = await fetch(`https://e-commerce-db-65ce.onrender.com/products/${ids}`).then(r => r.json());
+     const [{ id, title, description, imageUrl, price, stock, category }] = await fetch(`https://e-commerce-db-65ce.onrender.com/products/${ids}`).then(r => r.json());
 
      const data = {
           id, title, description, imageUrl, price, stock, category
