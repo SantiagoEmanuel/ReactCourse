f2d459253293a71dc0334371745845211167723e
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-08 19:09:34 -0300

    Remove function add for problems with backend whit CORS, i throw resolve that later...

---
+++ src/components/ui/item/ItemCount.jsx
@@ -0,0 +1,18 @@
+import { useCount } from '../../../hook/useCount'
+
+export function ItemCount({ stock, initial = 0 }) {
+     const { count, add, remove } = useCount(initial, stock);
+
+     return (
+          <div className='flex flex-col gap-4'>
+               <div className='flex items-center justify-between px-4'>
+                    <button onClick={remove} disabled={count < 1} className='hover:bg-[#212121] hover:text-white disabled:hover:text-[#ccc] disabled:text-[#808080] disabled:hover:bg-[#909090] px-4 py-2 text-xl font-bold transition-colors rounded-sm' >-</button>
+                    {count}
+                    <button onClick={add} disabled={count == stock} className='hover:bg-[#212121] hover:text-white disabled:hover:text-[#ccc] disabled:text-[#808080] disabled:hover:bg-[#909090] px-4 py-2 text-xl font-bold transition-colors rounded-sm' >+</button>
+               </div>
+               <button disabled={count < 1} className='hover:bg-[#212121] hover:text-white disabled:hover:text-[#ccc] disabled:text-[#808080] disabled:hover:bg-[#909090] text-xl font-bold transition-colors rounded-sm py-4 rounded-b-lg' >
+                    Add to cart
+               </button>
+          </div>
+     )
+}
