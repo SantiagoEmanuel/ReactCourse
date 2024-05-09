dca942a839c6698220537c6c915944f5697ce40d
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-09 19:08:15 -0300

    Add useCartToggleContext

--- src/components/ui/item/ItemCount.jsx
+++ src/components/ui/item/ItemCount.jsx
@@ -1,7 +1,10 @@
 import { useCount } from '../../../hook/useCount'
+import { useCartToggleContext } from '../../../hook/useCartToggleContext'
 
-export function ItemCount({ stock, initial = 0 }) {
+export function ItemCount({ stock, initial = 0, productID = null }) {
      const { count, add, remove } = useCount(initial, stock);
+
+     const [{ addCart }] = useCartToggleContext()
 
      return (
           <div className='flex flex-col gap-4'>
@@ -10,7 +13,7 @@ export function ItemCount({ stock, initial = 0 }) {
                     {count}
                     <button onClick={add} disabled={count == stock} className='hover:bg-[#212121] hover:text-white disabled:hover:text-[#ccc] disabled:text-[#808080] disabled:hover:bg-[#909090] px-4 py-2 text-xl font-bold transition-colors rounded-sm' >+</button>
                </div>
-               <button disabled={count < 1} className='hover:bg-[#212121] hover:text-white disabled:hover:text-[#ccc] disabled:text-[#808080] disabled:hover:bg-[#909090] text-xl font-bold transition-colors rounded-sm py-4 rounded-b-lg' >
+               <button disabled={count < 1} className='hover:bg-[#212121] hover:text-white disabled:hover:text-[#ccc] disabled:text-[#808080] disabled:hover:bg-[#909090] text-xl font-bold transition-colors rounded-sm py-4 rounded-b-lg' onClick={() => addCart([{ id: productID, COUNT: count }])} >
                     Add to cart
                </button>
           </div>
