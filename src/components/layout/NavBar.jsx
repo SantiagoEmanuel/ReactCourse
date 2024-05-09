bcb4037ec84959fbff329735ba78cbf60851b2fd
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-09 19:06:41 -0300

    Code Optimice

--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -1,8 +1,8 @@
 import { Brand } from "./Brand"
 import { CategoriesList } from "./CategoriesList"
 import { CartWidget } from "../ui/CartWidget"
-import { useUserContext } from "../../context/userContext"
-import { useUserToggleContext } from '../../context/userToggleContext'
+import { useUserContext } from "../../hook/useUserContext"
+import { useUserToggleContext } from '../../hook/useUserToggleContext'
 import { Link } from "react-router-dom";
 import { MenuIcon } from "../ui/icons/MenuIcon"
 import { showMenu } from "../../functions/openAside"
@@ -10,14 +10,6 @@ import { showMenu } from "../../functions/openAside"
 export function NavBar() {
      const user = useUserContext();
      const logOut = useUserToggleContext();
-     const countProduct = []
-     let x = 0
-     user?.cart.map(({ COUNT }, index) => {
-          x += COUNT;
-          if (index == user.cart.length - 1) {
-               countProduct.push(x)
-          }
-     })
 
      return (
           <header className="flex items-center justify-between">
@@ -26,8 +18,8 @@ export function NavBar() {
                     <div className="max-md:hidden flex items-center justify-around gap-6">
                          <CategoriesList />
                          {user?.status == 'admin' ? <Link to={'/create-product'} className="hover:scale-110 font-bold transition-transform">Create Product</Link> : ''}
-                         <CartWidget count={countProduct.toString()} />
-                         {user == null ? <><Link to={'/login'} className="hover:scale-110 font-bold transition-transform">Login</Link><Link to={'/register'} className="hover:scale-110 font-bold transition-transform">Register</Link></> : <button to={'/logout'} className="hover:scale-110 font-bold transition-transform" onClick={logOut}>Log out</button>}
+                         <CartWidget />
+                         {user == null ? <><Link to={'/login'} className="hover:scale-110 font-bold transition-transform">Login</Link><Link to={'/register'} className="hover:scale-110 font-bold transition-transform">Register</Link></> : <button className="hover:scale-110 font-bold transition-transform" onClick={logOut}>Log out</button>}
                     </div>
                     <div className="max-md:flex items-center content-center hidden">
                          <button onClick={showMenu}>
