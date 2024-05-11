f1cdfc8c599fe8bdde2c2092516c75d21d11bcd6
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-11 20:22:57 -0300

    Change function name setUser to loginUser and add autocomplete

--- src/views/LoginView.jsx
+++ src/views/LoginView.jsx
@@ -1,11 +1,11 @@
 import { useUserToggleContext } from '../hook/useUserToggleContext'
 
 export function LoginView() {
-     const setUser = useUserToggleContext();
+     const { loginUser } = useUserToggleContext();
 
      const handleSubmit = (e) => {
           e.preventDefault();
-          setUser(e.target.username.value, e.target.password.value);
+          loginUser(e.target.username.value, e.target.password.value);
      }
 
 
@@ -15,11 +15,11 @@ export function LoginView() {
                <form onSubmit={handleSubmit} className="flex flex-col w-full max-w-screen-sm gap-4">
                     <label className="flex flex-col w-full" >
                          Username:
-                         <input type="text" name="username" className="px-4 py-2 border" placeholder="Patrocleo" />
+                         <input type="text" name="username" className="px-4 py-2 border" placeholder="Patrocleo" autoComplete='username' />
                     </label>
                     <label className="flex flex-col w-full" >
                          Password:
-                         <input type="password" name="password" className="px-4 py-2 border" placeholder="*********" />
+                         <input type="password" name="password" className="px-4 py-2 border" placeholder="*********" autoComplete="current-password" />
                     </label>
                     <button className="rounded-xl hover:bg-gray-300 hover:text-black py-2 text-xl font-bold transition-colors border-2">Log in</button>
                </form>
