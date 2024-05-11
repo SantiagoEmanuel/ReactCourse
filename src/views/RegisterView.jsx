e1a8866cc259ec2f4fee87770e92951ffb7ebddd
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-11 20:24:37 -0300

    add 'toLowerCase()' in username, change h2 content, add required in all inputs and add autocomplete in username and password

--- src/views/RegisterView.jsx
+++ src/views/RegisterView.jsx
@@ -12,7 +12,7 @@ export function RegisterView() {
                     'Content-Type': 'application/json',
                },
                body: JSON.stringify({
-                    username: e.target.username.value,
+                    username: e.target.username.value.toLowerCase(),
                     first_name: e.target.first_name.value,
                     last_name: e.target.last_name.value,
                     email: e.target.email.value,
@@ -30,29 +30,29 @@ export function RegisterView() {
 
      return (
           <section className=" flex flex-col items-center w-full gap-8">
-               <h2 className="text-3xl text-orange-400">Register</h2>
+               <h2 className="text-3xl text-orange-600">Create your user</h2>
                <form onSubmit={handleSubmit} className="flex flex-col w-full max-w-screen-sm gap-4">
                     <label className="flex flex-col w-full gap-1">
                          Username
-                         <input className="w-full p-2 border outline-none" type="text" name="username" placeholder="username" />
+                         <input className="w-full p-2 border rounded-sm outline-none" min={8} max={20} type="text" name="username" placeholder="username" autoComplete='username' required />
                     </label>
                     <label className="flex flex-col w-full gap-1">
                          First Name
-                         <input className="w-full p-2 border outline-none" type="text" name="first_name" placeholder="first name" />
+                         <input className="w-full p-2 border rounded-sm outline-none" type="text" name="first_name" placeholder="first name" required />
                     </label>
                     <label className="flex flex-col w-full gap-1">
                          Last Name
-                         <input className="w-full p-2 border outline-none" type="text" name="last_name" placeholder="last name" />
+                         <input className="w-full p-2 border rounded-sm outline-none" type="text" name="last_name" placeholder="last name" required />
                     </label>
                     <label className="flex flex-col w-full gap-1">
                          Email
-                         <input className="w-full p-2 border outline-none" type="email" name="email" placeholder="email" />
+                         <input className="w-full p-2 border rounded-sm outline-none" type="email" name="email" placeholder="email" required />
                     </label>
                     <label className="flex flex-col w-full gap-1">
                          Password
-                         <input className="w-full p-2 border outline-none" type="password" name="password" placeholder="password" />
+                         <input className="w-full p-2 border rounded-sm outline-none" min={8} max={64} type="password" name="password" placeholder="password" autoComplete='current-password' required />
                     </label>
-                    <button className="rounded-xl py-4 text-xl text-orange-400 border">Create user</button>
+                    <button className="rounded-xl hover:bg-gray-300 hover:text-black py-2 text-xl transition-colors border-2">Create user</button>
                </form>
           </section>
      )
