41e15767afd23870620bd16842f38bcef9386484
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-11 20:15:05 -0300

    rename variable "productsIds" at "quantity", remove unnecessary code in addCart function, add "quantity" on value in CartContextProvider and fixed how its values are passed to CartContext

--- src/provider/CartProvider.jsx
+++ src/provider/CartProvider.jsx
@@ -3,48 +3,32 @@ import { CartContext } from '../context/CartContext';
 import { CartToggleContext } from '../context/CartToggleContext';
 
 export function CartProvider({ children }) {
+     const [quantity, setQuantity] = useState(0);
      const [cart, setCart] = useState(null);
-     const [productsIds, setProductsIds] = useState([]);
 
      const addCart = (userCart) => {
-          if (cart == null) {
-               const ids = []
-               let x = 0
-               userCart?.map(({ id, COUNT }, index) => {
-                    ids.push(id)
-                    x += COUNT
-                    if (index == userCart.length - 1) {
-                         setProductsIds(ids);
-                         setCart(x)
-                         return
-                    }
-               })
-          } else {
-               const ids = productsIds.filter(id => id != userCart[0].id)
-               let x = cart
-               userCart?.map(({ id, COUNT }, index) => {
-                    if (!ids.some(i => i === id)) {
-                         ids.push(id)
-                    }
-                    x += COUNT
-                    if (index == userCart.length - 1) {
-                         setProductsIds(ids);
-                         setCart(x)
-                         return
-                    }
-               })
-               return
-          }
+          let newCart = cart || {}
+          userCart.map(({ id, COUNT }) => {
+               newCart[id] = COUNT
+          })
+          let values = Object.values(newCart)
+          setCart(newCart);
+          setQuantity(values.reduce(
+               (accumulator, currentValue) => accumulator + currentValue,
+               0,
+          ))
+          return
      }
 
      const removeCart = () => {
+          setQuantity(0)
           setCart(null)
-          setProductsIds([])
+          return
      }
 
      return (
-          <CartContext.Provider value={[{ cart, productsIds }]}>
-               <CartToggleContext.Provider value={[{ addCart, removeCart }]}>
+          <CartContext.Provider value={{ quantity, cart }}>
+               <CartToggleContext.Provider value={{ addCart, removeCart }}>
                     {children}
                </CartToggleContext.Provider>
           </CartContext.Provider>
