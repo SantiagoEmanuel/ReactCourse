cb07f656798ff7e739e23f56f0f16c62c432428f
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-12 21:41:30 -0300

    Remove hook useProduct and use useProductContext, import arrow left and filter product with id

--- src/views/ItemDetailsContainer.jsx
+++ src/views/ItemDetailsContainer.jsx
@@ -1,15 +1,19 @@
+import { Link } from 'react-router-dom';
 import { ItemDetails } from '../components/ui/item/ItemDetails'
-import { useProduct } from '../hook/useProduct'
 import { useParams } from 'react-router-dom';
+import { ArrowLeft } from '../components/ui/icons/ArrowLeft';
+import { useProductContext } from '../hook/useProductContext';
 
 export function ItemDetailsContainer() {
      const { id } = useParams()
-     const { product } = useProduct(id);
-
+     const products = useProductContext()
      return (
           <section>
+               <Link to={'/'} className='flex items-center gap-2 text-xl font-bold'><ArrowLeft /> Back</Link>
                {
-                    <ItemDetails key={product.id} id={product.id} title={product.title} price={product.price} stock={product.stock} imgUrl={product.imageUrl} description={product.description} />
+                    products.map((product) => (
+                         product.id == id && <ItemDetails key={product.id} id={product.id} title={product.title} price={product.price} stock={product.stock} imgUrl={product.imageUrl} description={product.description} />
+                    ))
                }
           </section>
      )
