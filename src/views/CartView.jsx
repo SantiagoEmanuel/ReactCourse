8f760890af47000f1b61d1e873a765600ceffb83
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-12 22:29:44 -0300

    add responsive design

--- src/views/CartView.jsx
+++ src/views/CartView.jsx
@@ -31,13 +31,13 @@ export function CartView() {
                               <article className="flex items-center justify-between overflow-hidden border rounded-md" key={id}>
                                    <img
                                         src={imageUrl}
-                                        className="h-[200px] rounded-l-sm aspect-square"
+                                        className="h-[200px] rounded-l-sm aspect-square max-sm:h-[80px]"
                                    />
                                    <div className="flex justify-between flex-1 p-4">
-                                        <h4>{title}</h4>
+                                        <h4 className="max-md:text-xs text-xl">{title}</h4>
                                         <div className="flex items-center gap-4">
-                                             <p className="text-xl font-normal">{count}</p>
-                                             <button className="hover:bg-red-600 hover:text-white fond-bold px-4 py-1 text-red-600 transition-colors border border-red-600 rounded-md" onClick={() => removeItem(id)}>
+                                             <p className="max-sm:text-xs text-xl font-normal">{count}</p>
+                                             <button className="hover:bg-red-600 hover:text-white fond-bold max-sm:py-2 max-sm:text-xs px-4 py-1 text-red-600 transition-colors border border-red-600 rounded-md" onClick={() => removeItem(id)}>
                                                   Delete
                                              </button>
                                         </div>
