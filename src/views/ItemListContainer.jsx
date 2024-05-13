47780d1eefb14064d619da74cc87300683b07d6e
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-12 21:41:54 -0300

    remove useProduct and add useProductContext

--- src/views/ItemListContainer.jsx
+++ src/views/ItemListContainer.jsx
@@ -1,18 +1,20 @@
 import { useParams } from 'react-router-dom';
-import { useProducts } from '../hook/useProducts'
 import { ItemList } from '../components/containers/ItemList'
+import { useProductContext } from '../hook/useProductContext'
 
 export function ItemListContainer() {
      const { category } = useParams();
-     const { products } = useProducts(category);
-
+     const products = useProductContext()
      return (
           <section>
                {
-                    products.length == 0 && category == null ? <h2 className='text-3xl font-bold text-center'>Loading...</h2> : ''
+                    products.length == 0 && <h2 className='text-3xl font-bold text-center'>LOADING...</h2>
                }
                {
-                    products?.data == 0 && category != null ? <h2 className='text-2xl font-bold text-center'>Sorry, i don't have products for {category} for now.</h2> : <ItemList items={products} />
+                    products.length == 0 && category == null && <h2 className='text-2xl font-bold text-center'>Sorry, i don't have products for {category} for now.</h2>
+               }
+               {
+                    products.length > 0 && <ItemList items={products} filter={category != null ? category : null} />
                }
           </section>
      )
