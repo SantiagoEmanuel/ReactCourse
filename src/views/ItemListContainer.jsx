10f5d8790a809338a518e59c025e887e2d7c5ef0
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-12 21:47:32 -0300

    Fix bug, show unexpected text when don't have any category selected

--- src/views/ItemListContainer.jsx
+++ src/views/ItemListContainer.jsx
@@ -11,7 +11,7 @@ export function ItemListContainer() {
                     products.length == 0 && <h2 className='text-3xl font-bold text-center'>LOADING...</h2>
                }
                {
-                    products.length == 0 && category == null && <h2 className='text-2xl font-bold text-center'>Sorry, i don't have products for {category} for now.</h2>
+                    products.length == 0 && category != null && <h2 className='text-2xl font-bold text-center'>Sorry, i don't have products for {category} for now.</h2>
                }
                {
                     products.length > 0 && <ItemList items={products} filter={category != null ? category : null} />
