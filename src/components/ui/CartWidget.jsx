4c58597d5391bfba7d5bbc73ecfdde3e43316ec5
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-12 22:13:47 -0300

    add close aside function

--- src/components/ui/CartWidget.jsx
+++ src/components/ui/CartWidget.jsx
@@ -1,11 +1,12 @@
 import { CartIcon } from './icons/CartIcon';
 import { useCartContext } from '../../hook/useCartContext'
 import { Link } from 'react-router-dom';
+import { closeAside } from '../../functions/closeAside';
 
 export function CartWidget() {
      const { quantity } = useCartContext();
      return (
-          <Link to={'/cart'} className='relative cursor-pointer'>
+          <Link to={'/cart'} className='relative cursor-pointer' onClick={closeAside}>
                <CartIcon width={24} height={24} />
                {quantity != 0 ? <span className='-right-2 -bottom-1 absolute content-center w-4 h-4 text-[10px] text-center bg-red-600 rounded-full'>{quantity}</span> : ''}
           </Link>
