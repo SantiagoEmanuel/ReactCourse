c30806dfe5bdd8488fe593b5a5e40ee5d1ec26ee
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-13 23:26:17 -0300

    Upgrade how ItemDetailsContainer filter product

--- src/views/ItemDetailsContainer.jsx
+++ src/views/ItemDetailsContainer.jsx
@@ -2,10 +2,12 @@ import { Link, useParams } from 'react-router-dom';
 import { ItemDetails } from '../components/ui/item/ItemDetails'
 import { ArrowLeft } from '../components/ui/icons/ArrowLeft';
 import { useProductContext } from '../hook/useProductContext';
+import { getItem } from '../functions/getItem';
 
 export function ItemDetailsContainer() {
      const { id } = useParams()
      const products = useProductContext()
+     const product = getItem(id, products)
      return (
           <section>
                <Link to={'/'} className='flex items-center gap-2 text-xl font-bold'>
@@ -13,19 +15,15 @@ export function ItemDetailsContainer() {
                     Back
                </Link>
                {
-                    products.map((product) => (
-                         product.id == id
-                         &&
-                         <ItemDetails
-                              key={product.id}
-                              id={product.id}
-                              title={product.title}
-                              price={product.price}
-                              stock={product.stock}
-                              imgUrl={product.imageUrl}
-                              description={product.description}
-                         />
-                    ))
+                    <ItemDetails
+                         key={product.id}
+                         id={product.id}
+                         title={product.title}
+                         price={product.price}
+                         stock={product.stock}
+                         imgUrl={product.imageUrl}
+                         description={product.description}
+                    />
                }
           </section>
      )
