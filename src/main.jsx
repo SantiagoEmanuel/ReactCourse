5740e09f6b532c4d2749bffd3e0f57a4883c5f3c
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-13 21:13:21 -0300

    Remove all routes, and import routes variable

--- src/main.jsx
+++ src/main.jsx
@@ -1,52 +1,9 @@
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { ItemListContainer } from './views/ItemListContainer'
-import { ItemDetailsContainer } from './views/ItemDetailsContainer'
-import { ErrorView } from './views/ErrorView'
-import { LoginView } from './views/LoginView'
-import { CreateProductView } from './views/CreateProductView'
-import { RegisterView } from './views/RegisterView'
-import { CartView } from './views/CartView'
-import { App } from './App'
+import { routes } from './routes/routes'
 import './css/index.css'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        path: '/',
-        element: <ItemListContainer />
-      },
-      {
-        path: '/category/:category',
-        element: <ItemListContainer />
-      },
-      {
-        path: '/item/:id',
-        element: <ItemDetailsContainer />
-      },
-      {
-        path: '/login',
-        element: <LoginView />
-      },
-      {
-        path: '/register',
-        element: <RegisterView />
-      },
-      {
-        path: '/create-product',
-        element: <CreateProductView />
-      },
-      {
-        path: '/cart',
-        element: <CartView />
-      }
-    ],
-    errorElement: <ErrorView />
-  },
-])
+const router = createBrowserRouter(routes)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />
