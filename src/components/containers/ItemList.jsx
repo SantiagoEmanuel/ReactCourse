d0af10d4de8c062dac262024b8860cc3f7dbdb49
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-13 23:18:31 -0300

    Product rendering is optimized.

--- src/components/containers/ItemList.jsx
+++ src/components/containers/ItemList.jsx
@@ -4,11 +4,14 @@ export function ItemList({ items, filter = null }) {
      return (
           <section className="max-sm:grid-cols-2 max-[424px]:grid-cols-1 grid grid-cols-3 gap-4">
                {
-                    items.map(({ id, title, imageUrl, price, stock, category }) => (
-                         filter != null && filter == category ?
+                    filter ?
+                         items.map(({ id, title, imageUrl, price, stock, category }) => (
+                              filter == category &&
                               <Item key={id} id={id} img={imageUrl} title={title} price={price} stock={stock} />
-                              : filter == null && <Item key={id} id={id} img={imageUrl} title={title} price={price} stock={stock} />
-                    ))
+                         ))
+                         : items.map(({ id, title, imageUrl, price, stock }) => (
+                              <Item key={id} id={id} img={imageUrl} title={title} price={price} stock={stock} />
+                         ))
                }
           </section>
      )
