81f5eaac9493a0df2f93c140ba270ccfb443537a
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-14 03:24:13 -0300

    Remove grid cols 1 when the width is 424

--- src/components/containers/ItemList.jsx
+++ src/components/containers/ItemList.jsx
@@ -2,7 +2,7 @@ import { Item } from "../ui/item/Item";
 
 export function ItemList({ items, filter = null }) {
      return (
-          <section className="max-sm:grid-cols-2 max-[424px]:grid-cols-1 grid grid-cols-3 gap-4">
+          <section className="max-md:grid-cols-2 grid grid-cols-3 gap-4">
                {
                     filter ?
                          items.map(({ id, title, imageUrl, price, stock, category }) => (
