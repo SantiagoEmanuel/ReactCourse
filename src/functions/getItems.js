a9b3cf27e232ca6f70a64d88ba54c2e0c9aee502
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-13 23:21:15 -0300

    Upgrade getItems function, remove category filter

--- src/functions/getItems.js
+++ src/functions/getItems.js
@@ -1,9 +1,4 @@
-export async function getItems(category) {
-     if (category == null) {
-          const data = await fetch(`https://e-commerce-db-65ce.onrender.com/products`).then(r => r.json())
-          return data;
-     } else {
-          const data = await fetch(`https://e-commerce-db-65ce.onrender.com/products/categories/${category}`).then(r => r.json());
-          return data;
-     }
+export async function getItems() {
+     const data = await fetch(`https://e-commerce-db-65ce.onrender.com/products`).then(r => r.json())
+     return data;
 }
