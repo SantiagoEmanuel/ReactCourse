2a7fd4d4f39de2f85d7a13930d51d8e7ea90f1ff
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-16 12:47:55 -0700

    format code and rename function to logout

--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -1,32 +1,53 @@
 import { Link } from "react-router-dom";
-import { useUserToggleContext } from '../../hook/useUserToggleContext'
-import { useUserContext } from "../../hook/useUserContext"
-import { showMenu } from "../../functions/openAside"
-import { CategoriesList } from "./CategoriesList"
-import { CartWidget } from "../ui/CartWidget"
-import { MenuIcon } from "../ui/icons/MenuIcon"
-import { Brand } from "./Brand"
+import { useUserToggleContext } from "../../hook/useUserToggleContext";
+import { useUserContext } from "../../hook/useUserContext";
+import { showMenu } from "../../functions/openAside";
+import { CategoriesList } from "./CategoriesList";
+import { CartWidget } from "../ui/CartWidget";
+import { MenuIcon } from "../ui/icons/MenuIcon";
+import { Brand } from "./Brand";
 
 export function NavBar() {
-     const user = useUserContext();
-     const { loginUser } = useUserToggleContext();
+  const user = useUserContext();
+  const { logout } = useUserToggleContext();
 
-     return (
-          <header className="flex items-center justify-between">
-               <Brand>E-Commerce {user && <p>Welcome {user.username}!</p>} </Brand>
-               <nav className="flex items-center justify-around gap-6">
-                    <div className="max-md:hidden flex items-center justify-around gap-6">
-                         <CategoriesList />
-                         {user?.status == 'admin' ? <Link to={'/create-product'} className="hover:scale-110 font-bold transition-transform">Create Product</Link> : ''}
-                         <CartWidget />
-                         {user == null ? <><Link to={'/login'} className="hover:scale-110 font-bold transition-transform">Login</Link><Link to={'/register'} className="hover:scale-110 font-bold transition-transform">Register</Link></> : <button className="hover:scale-110 font-bold transition-transform" onClick={loginUser}>Log out</button>}
-                    </div>
-                    <div className="max-md:flex items-center content-center hidden">
-                         <button onClick={showMenu}>
-                              <MenuIcon />
-                         </button>
-                    </div>
-               </nav>
-          </header>
-     )
+  return (
+    <header className="flex items-center justify-between">
+      <Brand>E-Commerce {user && <p>Welcome {user.email}!</p>} </Brand>
+      <nav className="flex items-center justify-around gap-6">
+        <div className="flex items-center justify-around gap-6 max-md:hidden">
+          <CategoriesList />
+          <CartWidget />
+          {user == null ? (
+            <>
+              <Link
+                to={"/login"}
+                className="font-bold transition-transform hover:scale-110"
+              >
+                Login
+              </Link>
+              <Link
+                to={"/register"}
+                className="font-bold  transition-transform hover:scale-110"
+              >
+                Register
+              </Link>
+            </>
+          ) : (
+            <button
+              className="font-bold transition-transform hover:scale-110"
+              onClick={logout}
+            >
+              Log out
+            </button>
+          )}
+        </div>
+        <div className="hidden content-center items-center max-md:flex">
+          <button onClick={showMenu}>
+            <MenuIcon />
+          </button>
+        </div>
+      </nav>
+    </header>
+  );
 }
