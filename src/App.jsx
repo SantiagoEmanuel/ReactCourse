046656f61e0243f8b36b6a188d8ede0097ef7f52
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-16 13:42:18 -0700

    remove "redirect" and show Firebase configuration. Vercel environments don't work or I don't know how to use them and that caused a problem with Firebase authentication with API and more keys.

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,8 @@ import { Outlet } from "react-router-dom";
 import { UserProvider } from "./provider/UserProvider";
 import { CartProvider } from "./provider/CartProvider";
 import { Layout } from "./components/layout/Layout";
-import { useLocation } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
 
 export function App() {
-  const location = useLocation();
-  const navigate = useNavigate();
-  useEffect(() => {
-    navigate(location.pathname);
-  }, []);
-
   return (
     <CartProvider>
       <UserProvider>
