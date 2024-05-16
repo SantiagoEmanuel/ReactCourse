7538fc1f58b2b6a0dc29d10a181466ae8f25350e
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-16 12:51:22 -0700

    remove product context and use hook useProduct with id param and format the code

--- src/views/ItemDetailsContainer.jsx
+++ src/views/ItemDetailsContainer.jsx
@@ -1,31 +1,29 @@
-import { Link, useParams } from 'react-router-dom';
-import { ItemDetails } from '../components/ui/item/ItemDetails'
-import { ArrowLeft } from '../components/ui/icons/ArrowLeft';
-import { useProductContext } from '../hook/useProductContext';
-import { getItem } from '../functions/getItem';
+import { Link, useParams } from "react-router-dom";
+import { useProducts } from "../hook/useProducts";
+import { ItemDetails } from "../components/ui/item/ItemDetails";
+import { ArrowLeft } from "../components/ui/icons/ArrowLeft";
 
 export function ItemDetailsContainer() {
-     const { id } = useParams()
-     const products = useProductContext()
-     const product = getItem(id, products)
+  const { id } = useParams();
+  const { products } = useProducts(id);
 
-     return (
-          <section>
-               <Link to={'/'} className='flex items-center gap-2 text-xl font-bold'>
-                    <ArrowLeft />
-                    Back
-               </Link>
-               {
-                    <ItemDetails
-                         key={product.id}
-                         id={product.id}
-                         title={product.title}
-                         price={product.price}
-                         stock={product.stock}
-                         imgUrl={product.imageUrl}
-                         description={product.description}
-                    />
-               }
-          </section>
-     )
+  return (
+    <section>
+      <Link to={"/"} className="flex items-center gap-2 text-xl font-bold">
+        <ArrowLeft />
+        Back
+      </Link>
+      {products?.map((product) => (
+        <ItemDetails
+          key={product.id}
+          id={product.id}
+          title={product.title}
+          price={product.price}
+          stock={product.stock}
+          imgUrl={product.imageUrl}
+          description={product.description}
+        />
+      ))}
+    </section>
+  );
 }
