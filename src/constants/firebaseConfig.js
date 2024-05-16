5c5d1487253118672a3075238f99e01f331bbd70
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-16 13:30:26 -0700

    displaying sensitive content.

--- src/constants/firebaseConfig.js
+++ src/constants/firebaseConfig.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID,
+  apiKey: "AIzaSyBbGQHq1Sr34hksEDP4xxB9ss69BOnX0aA",
+  authDomain: "e-commerce-4a780.firebaseapp.com",
+  projectId: "e-commerce-4a780",
+  storageBucket: "e-commerce-4a780.appspot.com",
+  messagingSenderId: "274048607920",
+  appId: "1:274048607920:web:e49aa9e49c6c9a0e437d75",
+  measurementId: "G-EXV9NK1M0F",
 };
