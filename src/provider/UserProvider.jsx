6c14fe61de5d075fa05d513daa8de99c41740a19
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-16 12:49:13 -0700

    remove intermediary functions and format the code

--- src/provider/UserProvider.jsx
+++ src/provider/UserProvider.jsx
@@ -1,32 +1,19 @@
-import { UserContext } from '../context/UserContext'
-import { UserToggleContext } from '../context/UserToggleContext'
-import { useCartToggleContext } from '../hook/useCartToggleContext'
+import { UserContext } from "../context/UserContext";
+import { UserToggleContext } from "../context/UserToggleContext";
 import { useUser } from "../hook/useUser";
 import { useCart } from "../hook/useCart";
-import { useCartContext } from "../hook/useCartContext";
 
 export function UserProvider({ children }) {
+  const { user, login, logout, createNewUser } = useUser();
+  const { upgradeCart } = useCart();
 
-     const { user, logUser, closeUser, createNewUser } = useUser()
-     const { upgradeCart } = useCart();
-     const { addCart, deleteCart } = useCartToggleContext();
-     const { cart } = useCartContext();
-
-     const loginUser = (username, password) => {
-          if (user == null) {
-               logUser(username, password, addCart)
-               return
-          } else {
-               closeUser(cart, upgradeCart, deleteCart)
-               return
-          }
-     }
-
-     return (
-          <UserContext.Provider value={user}>
-               <UserToggleContext.Provider value={{ loginUser, upgradeCart, createNewUser }}>
-                    {children}
-               </UserToggleContext.Provider>
-          </UserContext.Provider>
-     )
+  return (
+    <UserContext.Provider value={user}>
+      <UserToggleContext.Provider
+        value={{ login, logout, upgradeCart, createNewUser }}
+      >
+        {children}
+      </UserToggleContext.Provider>
+    </UserContext.Provider>
+  );
 }
