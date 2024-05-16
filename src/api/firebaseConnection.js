84698246c5e626d9c87339691f8e7c17101d47d1
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-16 12:58:21 -0700

    fix import

--- src/api/firebaseConnection.js
+++ src/api/firebaseConnection.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { firebaseConfig } from "../constants/constRedirect";
+import { firebaseConfig } from "../constants/firebaseConfig";
 
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
