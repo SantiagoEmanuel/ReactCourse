5b829002a3c35c1f70a54157b613a1a4353caec9
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-16 12:54:59 -0700

    Adaptation of the registration form to the new user registration method with firebase auth

--- src/views/RegisterView.jsx
+++ src/views/RegisterView.jsx
@@ -1,47 +1,47 @@
-import { useUserToggleContext } from '../hook/useUserToggleContext'
+import { useUserToggleContext } from "../hook/useUserToggleContext";
 
 export function RegisterView() {
+  const { createNewUser } = useUserToggleContext();
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-     const { createUser } = useUserToggleContext()
+    createNewUser(e.target.email.value.toLowerCase(), e.target.password.value);
+  };
 
-     const handleSubmit = (e) => {
-          e.preventDefault();
-
-          const username = e.target.username.value.toLowerCase()
-          const first_name = e.target.first_name.value
-          const last_name = e.target.last_name.value
-          const email = e.target.email.value
-          const password = e.target.password.value
-
-          createUser(username, first_name, last_name, email, password)
-     }
-
-     return (
-          <section className=" flex flex-col items-center w-full gap-8">
-               <h2 className="text-3xl text-center">Create your user</h2>
-               <form onSubmit={handleSubmit} className="flex flex-col w-full max-w-screen-sm gap-4">
-                    <label className="flex flex-col w-full gap-1">
-                         Username
-                         <input className="w-full p-2 border rounded-sm outline-none" min={8} max={20} type="text" name="username" placeholder="username" autoComplete='username' required />
-                    </label>
-                    <label className="flex flex-col w-full gap-1">
-                         First Name
-                         <input className="w-full p-2 border rounded-sm outline-none" type="text" name="first_name" placeholder="first name" required />
-                    </label>
-                    <label className="flex flex-col w-full gap-1">
-                         Last Name
-                         <input className="w-full p-2 border rounded-sm outline-none" type="text" name="last_name" placeholder="last name" required />
-                    </label>
-                    <label className="flex flex-col w-full gap-1">
-                         Email
-                         <input className="w-full p-2 border rounded-sm outline-none" type="email" name="email" placeholder="email" required />
-                    </label>
-                    <label className="flex flex-col w-full gap-1">
-                         Password
-                         <input className="w-full p-2 border rounded-sm outline-none" min={8} max={64} type="password" name="password" placeholder="password" autoComplete='current-password' required />
-                    </label>
-                    <button className="rounded-xl hover:bg-gray-300 hover:text-black py-2 text-xl transition-colors border-2">Create user</button>
-               </form>
-          </section>
-     )
+  return (
+    <section className=" flex w-full flex-col items-center gap-8">
+      <h2 className="text-center text-3xl">Create your user</h2>
+      <form
+        onSubmit={handleSubmit}
+        className="flex w-full max-w-screen-sm flex-col gap-4"
+      >
+        <label className="flex w-full flex-col gap-1">
+          Email
+          <input
+            className="w-full rounded-sm border p-2 outline-none"
+            type="email"
+            name="email"
+            placeholder="email"
+            required
+          />
+        </label>
+        <label className="flex w-full flex-col gap-1">
+          Password
+          <input
+            className="w-full rounded-sm border p-2 outline-none"
+            min={8}
+            max={64}
+            type="password"
+            name="password"
+            placeholder="password"
+            autoComplete="current-password"
+            required
+          />
+        </label>
+        <button className="rounded-xl border-2 py-2 text-xl transition-colors hover:bg-gray-300 hover:text-black">
+          Create user
+        </button>
+      </form>
+    </section>
+  );
 }
