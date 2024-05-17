049875317cfcb2f45122be6f005b2b41042e3301
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-17 19:10:28 -0300

    Upgrade hook useUser

--- src/hook/useUser.js
+++ src/hook/useUser.js
@@ -4,24 +4,54 @@ import { redirects } from "../constants/redirects";
 import { logoutUser } from "../functions/logoutUser";
 import { loginUser } from "../functions/loginUser";
 import { createUser } from "../functions/createUser";
+import { useCartToggleContext } from "../hook/useCartToggleContext";
+import { createCart } from "../functions/createCart";
+import { generateOrder } from "../functions/generateOrder";
+import { toastNotification } from "../functions/toastNotification";
 
 export const useUser = () => {
   const [user, setUser] = useState(null);
   const navigate = useNavigate();
+  const { addCart, updateCart, deleteCart } = useCartToggleContext();
 
   const login = async (email, password) => {
-    setUser(await loginUser(email, password));
+    const us = await loginUser(email, password);
+    setUser(us);
+    if (us.cart[0].cart.length > 0) {
+      addCart(us.cart[0].cart);
+    }
     navigate(redirects.toHome);
   };
 
   const logout = () => {
+    updateCart(user.uid);
     setUser(logoutUser());
     navigate(redirects.toHome);
   };
 
   const createNewUser = async (email, password) => {
-    setUser(await createUser(email, password));
+    const us = await createUser(email, password);
+    createCart(us.uid);
+    setUser(us);
     navigate(redirects.toHome);
+  };
+
+  const comprarCarrito = (cart, total) => {
+    if (user == null) {
+      toastNotification(
+        "Error, necesitas iniciar sesion para realizar una compra.",
+      );
+      return;
+    }
+
+    const order = {
+      buyer: user?.email,
+      cart: cart,
+      total: total,
+    };
+    toastNotification("¡Carrito Comprado!");
+    generateOrder(order);
+    deleteCart();
   };
 
   return {
@@ -29,5 +59,6 @@ export const useUser = () => {
     login,
     logout,
     createNewUser,
+    comprarCarrito,
   };
 };
