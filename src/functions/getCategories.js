e50bf6284e2c9a0ac4c4132cc2037192e3c7cf31
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-17 19:12:03 -0300

    Rename products for categories

--- src/functions/getCategories.js
+++ src/functions/getCategories.js
@@ -4,11 +4,11 @@ import { db } from "../api/firebaseConnection";
 export async function getCategories() {
   try {
     const querySnapshot = await getDocs(collection(db, "categories"));
-    const products = querySnapshot.docs.map((doc) => ({
+    const categories = querySnapshot.docs.map((doc) => ({
       id: doc.id,
       ...doc.data(),
     }));
-    return products;
+    return categories;
   } catch (err) {
     console.error("Error getting categories:", err);
   }
