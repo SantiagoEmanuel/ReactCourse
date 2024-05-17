1722aa3f74ca63a13c9675475603e27eb6bbd5f3
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-17 19:05:21 -0300

    Create Footer component

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,11 @@
+export function Footer() {
+  return (
+    <footer>
+      <p className="text-balance text-center text-sm">
+        E-Commerce created by{" "}
+        <span className="font-bold text-orange-600">Santiago Emanuel</span> ||
+        CoderHouse React Course
+      </p>
+    </footer>
+  );
+}
