c7d6cfa442cb10a3c42a99217ae0b1c2a6f6c35b
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-17 19:11:35 -0300

    Upgrade loginUser function

--- src/functions/loginUser.js
+++ src/functions/loginUser.js
@@ -1,11 +1,18 @@
 import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../api/firebaseConnection";
+import { auth, db } from "../api/firebaseConnection";
+import { doc, getDoc } from "firebase/firestore";
 
 export const loginUser = async (email, password) => {
   const userCredentials = await signInWithEmailAndPassword(
     auth,
     email,
-    password
+    password,
   );
-  return userCredentials.user;
+  const querySnapshot = await getDoc(doc(db, "cart", userCredentials.user.uid));
+  const cart = [
+    {
+      ...querySnapshot.data(),
+    },
+  ];
+  return { ...userCredentials.user, cart: cart };
 };
