9626cbf9304943fd000db28da9561f05ee360713
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-17 19:08:50 -0300

    Add updateCart

--- src/provider/CartProvider.jsx
+++ src/provider/CartProvider.jsx
@@ -3,12 +3,13 @@ import { CartToggleContext } from "../context/CartToggleContext";
 import { useCart } from "../hook/useCart";
 
 export function CartProvider({ children }) {
-  const { cart, quantity, addCart, deleteCart, deleteItemCart } = useCart();
+  const { cart, quantity, addCart, deleteCart, deleteItemCart, updateCart } =
+    useCart();
 
   return (
     <CartContext.Provider value={{ quantity, cart }}>
       <CartToggleContext.Provider
-        value={{ addCart, deleteCart, deleteItemCart }}
+        value={{ addCart, deleteCart, deleteItemCart, updateCart }}
       >
         {children}
       </CartToggleContext.Provider>
