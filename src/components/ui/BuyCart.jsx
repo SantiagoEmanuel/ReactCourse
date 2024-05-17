3f9ad306b6d4ef4d0af7a0b6800cb73f4ff6536a
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-17 19:05:56 -0300

    Create BuyCart component

---
+++ src/components/ui/BuyCart.jsx
@@ -0,0 +1,19 @@
+import { useUserToggleContext } from "../../hook/useUserToggleContext";
+
+export function BuyCart({ cart, total }) {
+  const { comprarCarrito } = useUserToggleContext();
+
+  return (
+    <>
+      <button
+        className=" rounded-sm border-white bg-green-600 py-2 text-base font-bold"
+        title="Generate Order"
+        onClick={() => {
+          comprarCarrito(cart, total);
+        }}
+      >
+        Generate Order
+      </button>
+    </>
+  );
+}
