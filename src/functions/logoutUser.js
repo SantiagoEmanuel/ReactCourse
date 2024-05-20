3aea39364a5df4e223a75acb2ed8a94948c8af76
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-20 14:41:30 -0300

    remove logout function.

--- src/functions/logoutUser.js
+++ src/functions/logoutUser.js
@@ -2,11 +2,5 @@ import { signOut } from "firebase/auth";
 import { auth } from "../api/firebaseConnection";
 
 export const logoutUser = () => {
-  signOut(auth)
-    .then(() => {
-      return null;
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+  return null;
 };
