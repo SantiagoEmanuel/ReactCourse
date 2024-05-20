53dfc28d8511714377a51e9c7dbd68df4400c509
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-20 06:36:34 -0300

    add info

--- src/functions/createUser.js
+++ src/functions/createUser.js
@@ -1,7 +1,11 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../api/firebaseConnection";
+import { auth, db } from "../api/firebaseConnection";
+import { doc, setDoc } from "firebase/firestore";
 
-export const createUser = async (email, password) => {
+export const createUser = async (email, password, info) => {
   const result = await createUserWithEmailAndPassword(auth, email, password);
+
+  setDoc(doc(db, "userInfo", result.user.uid), info);
+
   return result.user;
 };
