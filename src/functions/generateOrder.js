55379431fca1305498d61188afc5f0c9b0449007
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-20 06:40:14 -0300

    upgrade function

--- src/functions/generateOrder.js
+++ src/functions/generateOrder.js
@@ -1,7 +1,20 @@
-import { collection, doc, setDoc } from "firebase/firestore";
+import { doc, addDoc, setDoc, collection } from "firebase/firestore";
 import { db } from "../api/firebaseConnection";
 
-export const generateOrder = (order) => {
-  setDoc(doc(collection(db, "orders")), order);
-  return;
+export const generateOrder = async (order, user) => {
+  const data = await addDoc(collection(db, "orders"), order);
+  const newOrder = {};
+  newOrder[data.id] = [...order.products];
+  const newUserInfo = {
+    username: user.username,
+    first_name: user.first_name,
+    last_name: user.last_name,
+    avatar: user.avatar,
+    orders:
+      user.orders != null
+        ? [newOrder, ...(Array.isArray(user.orders) && user.orders.flat())]
+        : [newOrder],
+  };
+  setDoc(doc(db, "userInfo", user.uid), newUserInfo);
+  return data.id;
 };
