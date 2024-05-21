1db5d1b9032db1a77f34efe6f583bb24fee77292
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-21 19:15:21 -0300

    Add validations

--- src/views/LoginView.jsx
+++ src/views/LoginView.jsx
@@ -1,11 +1,41 @@
+import { useState } from "react";
 import { useUserToggleContext } from "../hook/useUserToggleContext";
+import { loginValidateUser } from "../schema/user";
 
 export function LoginView() {
   const { login } = useUserToggleContext();
+  const [inputErrorMessage, setInputErrorMessage] = useState({
+    email: "",
+    password: "",
+  });
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    login(e.target.email.value.toLowerCase(), e.target.password.value);
+
+    const input = {
+      email: e.target.email.value.toLowerCase(),
+      password: e.target.password.value,
+    };
+
+    const result = loginValidateUser({ input: input });
+
+    if (result.success) {
+      login(e.target.email.value.toLowerCase(), e.target.password.value);
+      return;
+    }
+
+    setInputErrorMessage({
+      email: "",
+      password: "",
+    });
+
+    const newErrors = {};
+    result.error.errors.map((err) => {
+      document.getElementById(err.path[0]).classList.add("border-red-500");
+      newErrors[err.path[0]] = err.message;
+    });
+
+    setInputErrorMessage(newErrors);
   };
 
   return (
@@ -18,22 +48,30 @@ export function LoginView() {
         <label className="flex w-full flex-col">
           E-mail:
           <input
+            id="email"
             type="text"
             name="email"
             className="border px-4 py-2"
             placeholder="Patrocleo@protrocleo.com"
             autoComplete="email"
           />
+          <span className=" max-sm:text-sm">
+            {inputErrorMessage.email != "" && inputErrorMessage.email}
+          </span>
         </label>
         <label className="flex w-full flex-col">
           Password:
           <input
+            id="password"
             type="password"
             name="password"
             className="border px-4 py-2"
             placeholder="*********"
             autoComplete="current-password"
           />
+          <span className=" max-sm:text-sm">
+            {inputErrorMessage.password != "" && inputErrorMessage.password}
+          </span>
         </label>
         <button className="rounded-xl border-2 py-2 text-xl font-bold transition-colors hover:bg-gray-300 hover:text-black">
           Log in
--- src/views/RegisterView.jsx
+++ src/views/RegisterView.jsx
@@ -1,22 +1,64 @@
+import { useState } from "react";
 import { useUserToggleContext } from "../hook/useUserToggleContext";
+import { creationValidateUser } from "../schema/user";
 
 export function RegisterView() {
   const { createNewUser } = useUserToggleContext();
+  const [inputErrorMessage, setInputErrorMessage] = useState({
+    email: "",
+    username: "",
+    password: "",
+    avatar: "",
+    first_name: "",
+    last_name: "",
+  });
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    const info = {
+    const input = {
       username: e.target.username.value,
       first_name: e.target.first_name.value,
       last_name: e.target.last_name.value,
-      avatar: e.target.avatarUrl.value,
+      avatar: e.target.avatar.value,
+      password: e.target.password.value,
+      email: e.target.email.value.toLowerCase(),
     };
 
-    createNewUser(
-      e.target.email.value.toLowerCase(),
-      e.target.password.value,
-      info,
-    );
+    const result = creationValidateUser({ input: input });
+
+    if (result.success) {
+      const info = {
+        username: e.target.username.value,
+        first_name: e.target.first_name.value,
+        last_name: e.target.last_name.value,
+        avatar: e.target.avatar.value,
+      };
+
+      createNewUser(
+        e.target.email.value.toLowerCase(),
+        e.target.password.value,
+        info,
+      );
+      return;
+    }
+
+    setInputErrorMessage({
+      email: "",
+      username: "",
+      password: "",
+      avatar: "",
+      first_name: "",
+      last_name: "",
+    });
+
+    const newErrors = {};
+
+    result.error.errors.map((err) => {
+      document.getElementById(err.path[0]).classList.add("border-red-500");
+      newErrors[err.path[0]] = err.message;
+    });
+    setInputErrorMessage(newErrors);
   };
 
   return (
@@ -29,16 +71,20 @@ export function RegisterView() {
         <label className="flex w-full flex-col gap-1">
           Email
           <input
+            id="email"
             className="w-full rounded-sm border p-2 outline-none"
             type="email"
             name="email"
             placeholder="email"
-            required
           />
+          <span className=" font-semibold max-sm:text-xs">
+            {inputErrorMessage.email != "" && inputErrorMessage.email}
+          </span>
         </label>
         <label className="flex w-full flex-col gap-1">
           Password
           <input
+            id="password"
             className="w-full rounded-sm border p-2 outline-none"
             min={8}
             max={64}
@@ -46,47 +92,66 @@ export function RegisterView() {
             name="password"
             placeholder="password"
             autoComplete="current-password"
-            required
           />
+          <span className=" font-semibold max-sm:text-xs">
+            {inputErrorMessage.password != "" && inputErrorMessage.password}
+          </span>
         </label>
         <div className="flex gap-4 max-[500px]:flex-col">
           <label className="flex w-full flex-col gap-1">
             Username
             <input
+              id="username"
               className="w-full rounded-sm border p-2 outline-none"
               type="text"
               name="username"
               placeholder="Patrocleo"
             />
+            <span className=" font-semibold max-sm:text-xs">
+              {inputErrorMessage.username != "" && inputErrorMessage.username}
+            </span>
           </label>
           <label className="flex w-full flex-col gap-1">
             Avatar Url
             <input
+              id="avatar"
               className="w-full rounded-sm border p-2 outline-none"
               type="text"
-              name="avatarUrl"
+              name="avatar"
               placeholder="Patrocleo"
             />
+            <span className=" font-semibold max-sm:text-xs">
+              {inputErrorMessage.avatar != "" && inputErrorMessage.avatar}
+            </span>
           </label>
         </div>
         <div className="flex gap-4 max-[500px]:flex-col">
           <label className="flex w-full flex-col gap-1">
             First Name
             <input
+              id="first_name"
               className="w-full rounded-sm border p-2 outline-none"
               type="text"
               name="first_name"
               placeholder="Patrocleo"
             />
+            <span className=" font-semibold max-sm:text-xs">
+              {inputErrorMessage.first_name != "" &&
+                inputErrorMessage.first_name}
+            </span>
           </label>
           <label className="flex w-full flex-col gap-1">
             Last Name
             <input
+              id="last_name"
               className="w-full rounded-sm border p-2 outline-none"
               type="text"
               name="last_name"
               placeholder="Bimbo"
             />
+            <span className=" font-semibold max-sm:text-xs">
+              {inputErrorMessage.last_name != "" && inputErrorMessage.last_name}
+            </span>
           </label>
         </div>
         <button className="rounded-xl border-2 py-2 text-xl transition-colors hover:bg-gray-300 hover:text-black">
