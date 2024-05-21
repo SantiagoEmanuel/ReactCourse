7f21e04590ecc7f66ccdc3935b9bb637fc94f736
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-21 03:36:57 -0300

    Delete

--- src/functions/closeAside.js
+++
@@ -1,9 +0,0 @@
-export const closeAside = () => {
-  if (document.querySelector("aside").classList.contains("max-md:flex")) {
-    document.querySelector("aside").classList.remove("showAside");
-    document.querySelector("aside").classList.add("hiddenAside");
-    setTimeout(() => {
-      document.querySelector("aside").classList.remove("max-md:flex");
-    }, 200);
-  }
-};
--- src/functions/openAside.js
+++
@@ -1,9 +0,0 @@
-export const showMenu = () => {
-  const mobileMenu = document.querySelector("aside");
-  if (!mobileMenu.classList.contains("max-md:flex")) {
-    mobileMenu.classList.remove("hidden");
-    mobileMenu.classList.remove("hiddenAside");
-    mobileMenu.classList.add("max-md:flex");
-    mobileMenu.classList.add("showAside");
-  }
-};
