18b78daa6a6f0f6603b01803a3781e1031842c0e
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-21 03:16:38 -0300

    Remove imports and logic render in component, import new components (NavUserOptions)

--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -1,59 +1,25 @@
-import { Link } from "react-router-dom";
-import { useUserToggleContext } from "../../hook/useUserToggleContext";
 import { useUserContext } from "../../hook/useUserContext";
-import { showMenu } from "../../functions/openAside";
 import { CategoriesList } from "./CategoriesList";
 import { CartWidget } from "../ui/CartWidget";
-import { MenuIcon } from "../ui/icons/MenuIcon";
 import { Brand } from "./Brand";
+import { NavUserOptions } from "../ui/NavUserOptions";
 
 export function NavBar() {
   const user = useUserContext();
-  const { logout } = useUserToggleContext();
 
   return (
-    <header className="flex items-center justify-between">
-      <Brand>E-Commerce {user && <p>Welcome {user.first_name}!</p>} </Brand>
+    <header className="flex items-center justify-between p-4">
+      <Brand>
+        E-Commerce {user && <p className="text-sm">Welcome {user.username}!</p>}{" "}
+      </Brand>
       <nav className="flex items-center justify-around gap-6">
         <div className="flex items-center justify-around gap-6 max-md:hidden">
           <CategoriesList />
           <CartWidget />
-          {user == null ? (
-            <>
-              <Link
-                to={"/login"}
-                className="font-bold transition-transform hover:scale-110"
-              >
-                Login
-              </Link>
-              <Link
-                to={"/register"}
-                className="font-bold  transition-transform hover:scale-110"
-              >
-                Register
-              </Link>
-            </>
-          ) : (
-            <>
-              <Link
-                to={"/user"}
-                className="font-bold transition-transform hover:scale-110"
-              >
-                Profile
-              </Link>
-              <button
-                className="font-bold transition-transform hover:scale-110"
-                onClick={logout}
-              >
-                Log out
-              </button>
-            </>
-          )}
+          <NavUserOptions />
         </div>
         <div className="hidden content-center items-center max-md:flex">
-          <button onClick={showMenu}>
-            <MenuIcon />
-          </button>
+          <NavUserOptions />
         </div>
       </nav>
     </header>
