65f8a9680f1aa90b4787f7989fc7441106fd1274
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-21 20:17:11 -0300

    Add fix for avatars empty

--- src/schema/user.js
+++ src/schema/user.js
@@ -4,7 +4,7 @@ const createUserSchema = z.object({
   username: z.string().min(3),
   first_name: z.string().min(3),
   last_name: z.string().min(3),
-  avatar: z.string(),
+  avatar: z.string().url().optional().or(z.literal("")),
   email: z.string().email(),
   password: z.string().min(8).max(64),
 });
