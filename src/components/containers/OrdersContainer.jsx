b9c8d117ffe9b025f0ba16206745f1693cf43eab
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-21 16:07:28 -0300

    Use Material UI to show orders on accordion

--- src/components/containers/OrdersContainer.jsx
+++ src/components/containers/OrdersContainer.jsx
@@ -1,14 +1,80 @@
-import { OrderItem } from "../ui/order/OrderIem";
+import { useState } from "react";
+import { styled } from "@mui/material/styles";
+import ArrowForwardIosSharpIcon from "@mui/icons-material/ArrowForwardIosSharp";
+import MuiAccordion from "@mui/material/Accordion";
+import MuiAccordionSummary from "@mui/material/AccordionSummary";
+import MuiAccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import { OrderInfo } from "../ui/order/OrderInfo";
 
-export const OrdersContainer = ({ orders }) => {
+const Accordion = styled((props) => (
+  <MuiAccordion disableGutters elevation={0} square {...props} />
+))(({ theme }) => ({
+  border: `1px solid ${theme.palette.divider}`,
+  "&:not(:last-child)": {
+    borderBottom: 0,
+  },
+  "&::before": {
+    display: "none",
+  },
+}));
+
+const AccordionSummary = styled((props) => (
+  <MuiAccordionSummary
+    expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: "0.9rem" }} />}
+    {...props}
+  />
+))(({ theme }) => ({
+  backgroundColor:
+    theme.palette.mode === "dark"
+      ? "rgba(255, 255, 255, .05)"
+      : "rgba(0, 0, 0, .03)",
+  flexDirection: "row-reverse",
+  "& .MuiAccordionSummary-expandIconWrapper.Mui-expanded": {
+    transform: "rotate(90deg)",
+  },
+  "& .MuiAccordionSummary-content": {
+    marginLeft: theme.spacing(1),
+  },
+}));
+
+const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
+  padding: theme.spacing(2),
+  borderTop: "1px solid rgba(0, 0, 0, .125)",
+}));
+
+export function OrdersContainer({ orders }) {
+  const [expanded, setExpanded] = useState("panel1");
+
+  const handleChange = (panel) => (event, newExpanded) => {
+    setExpanded(newExpanded ? panel : false);
+  };
+
   return (
-    <>
-      <h3 className="text-center text-2xl">You orders</h3>
-      <section className="m-auto flex w-full max-w-[500px] flex-col gap-8 max-[370px]:m-0">
-        {orders.map(({ order, products }) => (
-          <OrderItem order={order} products={products} key={order} />
-        ))}
-      </section>
-    </>
+    <section className="m-auto max-w-[500px]">
+      <h2 className="text-xl">Your orders</h2>
+      {orders.map(({ order, products }, index) => (
+        <Accordion
+          expanded={expanded === `panel${index}`}
+          onChange={handleChange(`panel${index}`)}
+          key={order}
+        >
+          <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
+            <Typography>{order}</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            {products.map(({ id, imageUrl, title, price, count }) => (
+              <OrderInfo
+                key={order + id}
+                imageUrl={imageUrl}
+                title={title}
+                price={price}
+                count={count}
+              />
+            ))}
+          </AccordionDetails>
+        </Accordion>
+      ))}
+    </section>
   );
-};
+}
