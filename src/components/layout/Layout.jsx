9bea840a70ef92ea07968315be4df8c6688d97b2
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-21 03:13:30 -0300

    Add padding in main element and replace MobileMenu component to BottomNavigation

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,6 +1,6 @@
 import { Analytics } from "@vercel/analytics/react";
 import { NavBar } from "./NavBar";
-import { MobileMenu } from "./MobileMenu";
+import { BottomNavigation } from "./BottomNavigation";
 import { Footer } from "./Footer";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -9,11 +9,11 @@ export function Layout({ children }) {
   return (
     <>
       <NavBar />
-      <main className="flex flex-1 flex-col items-center justify-center">
+      <main className="flex flex-1 flex-col items-center justify-center p-4">
         {children}
       </main>
       <Footer />
-      <MobileMenu />
+      <BottomNavigation />
       <ToastContainer
         limit={6}
         position="bottom-right"
