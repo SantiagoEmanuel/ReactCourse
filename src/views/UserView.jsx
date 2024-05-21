ec837348027efee7a4589629869626c5bf903961
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-21 03:18:37 -0300

    Reduced logic for render in component, imported UserInfo and Orders component

--- src/views/UserView.jsx
+++ src/views/UserView.jsx
@@ -2,6 +2,8 @@ import { Link } from "react-router-dom";
 import { useUserContext } from "../hook/useUserContext";
 import { useState } from "react";
 import { useEffect } from "react";
+import { UserInfo } from "../components/UserInfo";
+import { Orders } from "../components/Orders";
 
 export function UserView() {
   const user = useUserContext();
@@ -23,6 +25,7 @@ export function UserView() {
       setOrders(data);
     }
   }, [user]);
+
   if (!user) {
     return (
       <div className="flex flex-col gap-4">
@@ -38,77 +41,10 @@ export function UserView() {
   }
 
   return (
-    <section className="flex w-full flex-col gap-10 rounded-xl border p-8">
-      <header className="m-auto max-w-[600px] ">
-        <div className="content-center">
-          {user.avatar ? (
-            <img
-              src={user.avatar}
-              alt={`${user.first_name} ${user.last_name} avatar`}
-              title={`${user.first_name} ${user.last_name} avatar`}
-              className="m-auto h-auto w-[200px] rounded-full"
-            />
-          ) : (
-            <img
-              src="/user-no-avatar.png"
-              alt="User avatar is not available"
-              className="m-auto w-[200px] rounded-full"
-            />
-          )}
-        </div>
-        <div className="flex flex-col gap-4 pt-4">
-          <h2 className=" text-center text-2xl font-normal">
-            {user.first_name} {user.last_name}
-          </h2>
-          <div className="flex flex-col items-center justify-center">
-            <p className="opacity-40">@{user.username}</p>
-            <p className="opacity-40">{user.email}</p>
-          </div>
-        </div>
-      </header>
-      <section>
-        {orders.length == 0 ? (
-          <h3 className="text-center text-xl">You don't have any order</h3>
-        ) : (
-          <>
-            <h3 className="text-center text-5xl">orders</h3>
-            <section className="m-auto flex max-w-[500px] flex-col gap-8">
-              {orders.map(({ order, products }) => (
-                <article
-                  key={order}
-                  className="flex flex-col gap-8 rounded-lg border p-4"
-                >
-                  <header>
-                    <h4 className="text-center text-2xl">Order: {order}</h4>
-                  </header>
-                  <section className=" flex items-center justify-center gap-8">
-                    {products?.map(({ id, imageUrl, title, price, count }) => (
-                      <article
-                        key={id}
-                        className="flex w-[200px] flex-col items-center"
-                      >
-                        <header>
-                          <img
-                            src={imageUrl}
-                            alt={title}
-                            width={100}
-                            className="aspect-square rounded-full"
-                          />
-                        </header>
-                        <section>
-                          <h5>{title}</h5>
-                          <p>
-                            {price} * {count} = {price * count}
-                          </p>
-                        </section>
-                      </article>
-                    ))}
-                  </section>
-                </article>
-              ))}
-            </section>
-          </>
-        )}
+    <section className="flex w-full flex-col gap-20">
+      <UserInfo user={user} />
+      <section className="w-full">
+        <Orders orders={orders} />
       </section>
     </section>
   );
