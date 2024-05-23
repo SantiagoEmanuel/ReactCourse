70ccbefaac2af133a2dec2a86779d0f9e711e68d
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-22 23:42:46 -0300

    remove itemlist and add show product

--- src/views/ItemDetailsContainer.jsx
+++ src/views/ItemDetailsContainer.jsx
@@ -1,29 +1,21 @@
 import { Link, useParams } from "react-router-dom";
 import { useProducts } from "../hook/useProducts";
-import { ItemDetails } from "../components/ui/item/ItemDetails";
 import { ArrowLeft } from "../components/ui/icons/ArrowLeft";
+import { ShowProduct } from "../components/ShowProduct";
 
 export function ItemDetailsContainer() {
   const { id } = useParams();
   const { products } = useProducts(id);
 
   return (
-    <section>
+    <section className="flex w-full flex-col gap-8">
       <Link to={"/"} className="flex items-center gap-2 text-xl font-bold">
         <ArrowLeft />
         Back
       </Link>
-      {products?.map((product) => (
-        <ItemDetails
-          key={product.id}
-          id={product.id}
-          title={product.title}
-          price={product.price}
-          stock={product.stock}
-          imgUrl={product.imageUrl}
-          description={product.description}
-        />
-      ))}
+      <section>
+        <ShowProduct products={products} />
+      </section>
     </section>
   );
 }
