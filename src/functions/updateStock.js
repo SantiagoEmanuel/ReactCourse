894bbce30785aef1c632127d12d15109c6dc8e98
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-22 23:40:08 -0300

    Add function to update stock

---
+++ src/functions/updateStock.js
@@ -0,0 +1,11 @@
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "../api/firebaseConnection";
+
+export function updateStock({ product, newStock }) {
+  console.log(product, newStock);
+  const newProduct = {
+    ...product,
+    stock: newStock,
+  };
+  updateDoc(doc(db, "products", product.id), newProduct);
+}
