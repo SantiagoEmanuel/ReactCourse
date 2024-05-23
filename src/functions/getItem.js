93b677818f788dd2244b405b9a256a806b3fc05c
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-22 23:39:48 -0300

    fix error to return a non-existent product

--- src/functions/getItem.js
+++ src/functions/getItem.js
@@ -4,15 +4,17 @@ import { db } from "../api/firebaseConnection";
 export async function getItem(id) {
   try {
     const querySnapshot = await getDoc(doc(db, "products", id));
-    const product = [
-      {
-        id: querySnapshot.id,
-        ...querySnapshot.data(),
-      },
-    ];
-    return product;
+    if (querySnapshot.data()) {
+      const product = [
+        {
+          id: querySnapshot.id,
+          ...querySnapshot.data(),
+        },
+      ];
+      return product;
+    }
+    return [];
   } catch (err) {
     console.error("Error getting item:", err);
-    return [];
   }
 }
