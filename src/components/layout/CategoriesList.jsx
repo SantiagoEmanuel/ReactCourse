ff7673185d1983698c5f398d9c73d0e9c2727a82
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-24 19:58:55 -0300

    remove ul

--- src/components/layout/CategoriesList.jsx
+++ src/components/layout/CategoriesList.jsx
@@ -1,15 +1,23 @@
 import { Link } from "react-router-dom";
 import { useCategories } from "../../hook/useCategories";
+import { Button } from "@mui/material";
 
-export function CategoriesList({ css = "flex items-center gap-4" }) {
+export function CategoriesList({
+  css = "flex items-center gap-4 max-md:hidden ",
+}) {
   const { categories } = useCategories();
   return (
-    <ul className={css}>
+    <div className={css}>
       {categories.map(({ name, url }, index) => (
-        <li key={index} className="transition-transform hover:scale-110">
-          <Link to={url}>{name}</Link>
-        </li>
+        <Button
+          className="hover:scale-110 hover:transition-transform"
+          key={index}
+        >
+          <Link className="font-code text-white" to={url}>
+            {name}
+          </Link>
+        </Button>
       ))}
-    </ul>
+    </div>
   );
 }
