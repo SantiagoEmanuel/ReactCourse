e7ea932801e40876b26e3a5273c101f6af92c8c1
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-24 20:28:18 -0300

    add padding

--- src/components/layout/BottomNavigation.jsx
+++ src/components/layout/BottomNavigation.jsx
@@ -7,7 +7,7 @@ export function BottomNavigation() {
   const { categories } = useCategories();
 
   return (
-    <Container className="fixed bottom-0 hidden w-full items-center justify-evenly gap-4 rounded-t-lg border bg-white pb-1 pl-1 pr-1 pt-2 text-black max-md:flex">
+    <Container className="fixed bottom-0 hidden w-full items-center justify-evenly gap-4 rounded-t-lg border bg-white pb-4 pl-1 pr-1 pt-2 text-black max-md:flex">
       <ButtonNavigation url={"/"}>INICIO</ButtonNavigation>
       {categories.map(({ name, url }) => (
         <ButtonNavigation url={url} key={name}>
