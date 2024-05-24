b01ff18fef5e827afb00a6fbab6c479b3f106604
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-24 19:57:38 -0300

    add css

--- src/components/ui/CartWidget.jsx
+++ src/components/ui/CartWidget.jsx
@@ -5,10 +5,10 @@ import { CartIcon } from "./icons/CartIcon";
 export function CartWidget() {
   const { quantity } = useCartContext();
   return (
-    <Link to={"/cart"} className="relative cursor-pointer">
+    <Link to={"/cart"} className="relative m-0 cursor-pointer p-0">
       <CartIcon width={24} height={24} />
       {quantity != 0 && (
-        <span className="absolute -bottom-1 -right-2 h-4 w-4 content-center rounded-full bg-red-600 text-center text-[10px]">
+        <span className="absolute -bottom-1 -right-2 h-4 w-4 content-center rounded-full bg-red-600 text-center text-[10px] text-white">
           {quantity}
         </span>
       )}
