2195bbdb9b1b4cc390dedb43833ca2ddac709657
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-24 19:56:42 -0300

    change section for footer

--- src/components/ui/order/OrderInfo.jsx
+++ src/components/ui/order/OrderInfo.jsx
@@ -9,12 +9,12 @@ export const OrderInfo = ({ imageUrl, title, price, count }) => {
           className="aspect-square h-auto"
         />
       </header>
-      <section className="flex w-full flex-col gap-2 pl-4 pr-2">
+      <footer className="flex w-full flex-col gap-2 pl-4 pr-2">
         <h5 className="text-balance">{title}</h5>
         <p className="text-sm">
           ${price} * {count} = ${price * count}
         </p>
-      </section>
+      </footer>
     </article>
   );
 };
